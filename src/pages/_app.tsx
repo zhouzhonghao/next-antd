e2d6bf3ef8673ef950ca3073952c45c030bca1cc
Bobby Zhou <zhouzhonghao@qq.com>
2023-11-27 17:34:50 +0800

    feat: add antd Hello, world!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import { ConfigProvider, Layout, theme } from 'antd'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider theme={{
+      algorithm: theme.darkAlgorithm
+    }}>
+      <Layout className="w-[100%] h-[100%]">
+        <Component {...pageProps} />
+      </Layout>
+    </ConfigProvider>
+  )
 }
